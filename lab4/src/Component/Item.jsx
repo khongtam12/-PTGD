2dcaa1965b17bb633a90db00e18dfaa52ae18e33
H82M19\Student <Student>
2025-03-05 20:23:29 +0700

    phan 1 lab4

--- lab4/src/Component/Item.jsx
+++ lab4/src/Component/Item.jsx
@@ -5,7 +5,7 @@ export default function Item({name,Image,Fave}){
             <img src={Image} alt="" width='250px' height='100px'/>
             <div id="title">
             <p>{name}</p>
-            <img src={Fave} alt="" width='20px' height='20px' />
+            <img src={Fave} alt="" width='25px' height='25px' />
             </div>
 
         </div>
